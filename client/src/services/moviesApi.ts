3de9674cbf6270829ce55763f91e92670a6459ef
gordun209 <gordun209@hotmail.com>
2022-04-19 11:12:54 +0300

    i

--- client/src/services/moviesApi.ts
+++ client/src/services/moviesApi.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 //    https://imdb-api.com/en/API/SearchMovie//inception 2010
 //https://imdb-api.com/en/API/Search/k_t0zj84io/${search}
-const apiKey = 'k_aaaaaaaa'
+//    https://imdb-api.com/en/API/SearchEpisode//London
+const apiKey = 'k_hd2hitvi'
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
