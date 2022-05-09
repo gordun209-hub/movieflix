090a9647cefdf6a4ea2aba663e7d894816bf4f46
gordun209 <gordun209@hotmail.com>
2022-05-09 11:51:14 +0300

    laa

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -3,15 +3,13 @@ describe('My First Test', () => {
 		cy.visit('http://localhost:3000')
 	})
 	it('clicks movies button and sees images', () => {
-		cy.get('[data-cy="movies"]').click()
-		cy.get('[data-cy="tt1877830"] > .bg-slate-500').should('be.visible').contains('The Batman')
+		cy.get('[href="/movies"]').click()
+		cy.get('[data-cy="tt1877830"] > .css-1jf8mhn').should('be.visible').contains('The Batman')
 		cy.scrollTo('bottom').url().should('include', '/movies')
 	})
 	it('clicks the action genre and sees movies', () => {
-		cy.get('[data-cy="movies"]', { timeout: 100000 }).click()
-		cy.get('[data-cy="action"]', { timeout: 100000 }).click()
-		cy.get('[data-cy="tt1877830"] > .bg-slate-500').should('be.visible').contains('The Batman')
-		cy.scrollTo('bottom').url().should('include', 'movies/action')
+		cy.get('[href="/movies"]').click()
+		cy.get('[data-cy="action"] > .MuiTypography-root').click()
 	})
 })
 
@@ -19,25 +17,14 @@ describe('can navigate between genres', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000')
 	})
-	it('clicks movies button and sees images', () => {
-		cy.get('[data-cy="movies"]', { timeout: 100000 }).click()
 
-		cy.scrollTo('bottom').url().should('include', '/movies')
-	})
-	it('clicks the action genre and sees movies', () => {
-		cy.get('[data-cy="movies"]').click()
-		cy.get('[data-cy="action"]').click()
-
-		cy.scrollTo('bottom').url().should('include', 'movies/action')
-	})
 	it('clicks the comedy genre and sees movies', () => {
-		cy.get('[data-cy="movies"]').click()
+		cy.get('[href="/movies"]').click()
 		cy.get('[data-cy="comedy"]').click()
-
 		cy.scrollTo('bottom').url().should('include', 'movies/comedy')
 	})
 	it('clicks the horror genre and sees movies', () => {
-		cy.get('[data-cy="movies"]').click()
+		cy.get('[href="/movies"]').click()
 		cy.get('[data-cy="horror"]').click()
 
 		cy.scrollTo('bottom').url().should('include', 'movies/horror')
--- declarations.d.ts
+++ declarations.d.ts
@@ -3,4 +3,4 @@
 declare module 'https://framer.com/m/*'
 // NOTE: The declaration below was injected by `"framer"`
 // see https://www.framer.com/docs/guides/handshake for more information.
-declare module "https://framer.com/m/*";
+declare module 'https://framer.com/m/*'
